c2b6949c2e0b2cd6bb8441c79b2adff70da31059
Poscat <poscat@mail.poscat.moe>
2020-05-31 21:53:48 +0800

    Implement tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,11 @@ module Main where
 
 import qualified Data.Text.IO as T
 import Language.TL.Parser
+import Language.TL.Types
 import Text.Megaparsec
 
 main :: IO ()
 main = do
   f <- T.readFile "test/data/td_api.tl"
-  parseTest program f
+  let Right res = runParser program "td_api.tl" f
+  print $ head res
