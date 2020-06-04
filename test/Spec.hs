596d5b878001bf65a328c16cdc9409e3a89ad66b
Poscat <poscat@mail.poscat.moe>
2020-06-04 18:29:19 +0800

    Add simple property test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import qualified Data.Text.IO as T
-import Language.TL.Parser
-import Language.TL.Types
-import Text.Megaparsec
+import Data.Aeson
+import Language.TL.I64
+import Test.QuickCheck
 
 main :: IO ()
 main = do
-  f <- T.readFile "test/data/td_api.tl"
-  let Right res = runParser program "td_api.tl" f
-  print $ head res
+  quickCheck bijection
+
+bijection :: I64 -> Property
+bijection i = decode (encode i) === Just i
