3d5593557073ff28748a939d73b0f09e374151e3
Poscat <poscat@poscat.moe>
2021-10-18 13:10:18 +0800

    fix test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,23 @@
 module Main where
 
-import Data.Aeson
-import Language.TL.I64
-import Test.QuickCheck
+import qualified Data.Text.IO as T
+import Language.TL.Parser
+import Text.Megaparsec
 
 main :: IO ()
 main = do
-  quickCheck bijection
-
-bijection :: I64 -> Property
-bijection i = decode (encode i) === Just i
+  f <- T.readFile "test/data/td_api.tl"
+  let r = runParser program "td_api" f
+  case r of
+    Left _ -> error "error: parse failed"
+    Right a -> print a
+  f2 <- T.readFile "test/data/telegram_api.tl"
+  let r2 = runParser program "telegram_api" f2
+  case r2 of
+    Left _ -> error "error: parse failed"
+    Right a -> print a
+  f3 <- T.readFile "test/data/mtproto_api.tl"
+  let r3 = runParser program "mtproto_api" f3
+  case r3 of
+    Left _ -> error "error: parse failed"
+    Right a -> print a
